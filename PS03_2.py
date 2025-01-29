d49efe3eae11180d13d4a5ee1ba369ca98a703d6
Andrea Hizon <202480050@psu.palawan.edu.ph>
2025-01-29 02:43:07 -1200

    Update PS03_2.py

--- PS03_2.py
+++ PS03_2.py
@@ -2,13 +2,13 @@ with open("output.txt", 'r') as file:
     lines = file.readlines()
     for line in lines:
         print(line.strip())
-
+#write_file.py
 with open('output.txt', 'w') as file:
     file.write("Hello, World!\n")
-    
+
 with open("output.txt", "a") as file:
     file.write("This is appended text.\n")
-
+#write_numbers
 with open("output.txt", "w") as file:
     for number in range(1, 11):
         file.write(f"{number}\n")
